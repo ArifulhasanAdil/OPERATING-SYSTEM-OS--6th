c3cb806121df2230daa8159103531cb5cf4952f6
Adil <123837714+ArifulhasanAdil@users.noreply.github.com>
2025-11-25 01:53:30 +0600

    Add files via upload

---
+++ Project/main.cpp
@@ -0,0 +1,19 @@
+#include "function.h"
+//#include <graphics.h>
+//#include <bits/stdc++.h>
+//#include <stdlib.h>
+//using namespace std;
+int main()
+{
+
+    //height and weight of main window
+    //width=800 and height=520
+
+    initwindow(800, 920,"Spartan Project");
+
+    login();//getting from function.h
+
+    getch();//freeze the screen
+
+    return 0;
+}
